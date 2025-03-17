3c5b8fe93752c891bb7302e0a93a31b81c00e039
Akshayba12 <akshaybajantri12@gmail.com>
2025-03-17 17:56:17 +0530

    changed the local url to production url

--- src/services/axiosinstance.ts
+++ src/services/axiosinstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://fancy-malabi-3dc5ba.netlify.app/',
     headers: {
         "Content-Type": "application/json"
     }
